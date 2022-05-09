07865deb01a9bc8627b55b5f6cf42c43a3bfbb05
barahana <barahana13@naver.com>
2022-05-09 12:25:56 +0900

    auto push

--- BOJ/1059.py
+++ BOJ/1059.py
@@ -3,4 +3,18 @@ a = list(map(int, input().split()))
 b = int(input())
 a.append(b)
 c = sorted(a)
-print(c[c.index(b)+1] - c[c.index(b)-1] - 2)
+count = 0
+if c.index(b) == 0:
+    for i in range(1,b+1):
+        for j in range(b, c[c.index(b)+1]):
+            if i==j:
+                continue
+            count+=1
+
+else:
+    for i in range(c[c.index(b)-1]+1,b+1):
+        for j in range(b,c[c.index(b)+1]):
+            if i==j:
+                continue
+            count+=1
+print(count)
