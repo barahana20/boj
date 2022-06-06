0f0965e5c904c7e1327b76bded9e8c4f4acea728
bara <barahana13@naver.com>
2022-06-06 20:08:06 +0900

    auto push

---
+++ BOJ/11718.py
@@ -0,0 +1,5 @@
+import sys
+
+a = sys.stdin.read()
+
+print(a)
---
+++ BOJ/24860.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().split())
+d = a*b
+a,b=map(int,input().split())
+d += a*b
+a,b,c=map(int,input().split())
+d *= (a*b*c)
+
+print(d)
