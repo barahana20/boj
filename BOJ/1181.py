c17c5da577826760f5083f4a91064f4f540d1811
bara <barahana13@naver.com>
2022-08-31 14:49:40 +0900

    auto push

--- BOJ/1181.py
+++ BOJ/1181.py
@@ -1,6 +1,17 @@
 a = int(input())
 
+def append_array(arr, i):
+    arr = list()
+    arr.append(i)
+    return arr
 b = [input() for _ in range(a)]
-b.sort()
 
-print(b)
+dic = {}
+
+for i in b:
+    dic[len(i)] = []
+
+for i in b:
+    dic[len(i)].append(i)
+
+print(dic)
