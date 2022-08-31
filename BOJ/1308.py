0760af0e79141d2bd9dd3bc519e8a4f4d3a85826
bara <barahana13@naver.com>
2022-08-31 15:50:10 +0900

    auto push

--- BOJ/1308.py
+++ BOJ/1308.py
@@ -1,11 +1,22 @@
 y1,m1,d1 = map(int, input().split())
 y2,m2,d2 = map(int, input().split())
 D_day = 0
+
 m_list = {1:31, 2:28, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:31, 12:30}
 m_list1 = {1:31, 2:29, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:31, 12:30} # 윤년
-
+def cal_leapyear(y1, y2):
+    result = 0
+    for i in range(y1, y2):
+        if i % 4 == 0:
+            if i % 400 == 0:
+                result += 366
+            elif i % 100 == 0:
+                result += 365
+            else:
+                result += 366
+    return result
 if m1 < m2:
-    D_day += (y2 - y1) * 365
+    D_day += cal_leapyear(y1, y2+1)
     if d1 < d2:
         for m in range(m1, m2):
             D_day += m_list[m]
@@ -13,11 +24,9 @@ if m1 < m2:
     elif d1 > d2:
         for m in range(m1, m2):
             D_day += m_list[m]
-        D_day += (d2 - d1 + 1)
-    if d1 < d2:
         D_day += (d2 - d1)
 elif m1 > m2:
-    D_day += (y2 - y1 - 1) * 365
+    D_day += cal_leapyear(y1, y2)
     if d1 < d2:
         for m in range(m1, 13):
             D_day += m_list[m]
