5149245bdde7e0ac30baeb3b2b17c5daf49f42c9
bara <barahana13@naver.com>
2022-08-31 15:40:05 +0900

    auto push

--- BOJ/1308.py
+++ BOJ/1308.py
@@ -6,19 +6,29 @@ m_list1 = {1:31, 2:29, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:31, 1
 
 if m1 < m2:
     D_day += (y2 - y1) * 365
-    for m in range(m1, m2):
-        D_day += m_list[m]
+    if d1 < d2:
+        for m in range(m1, m2):
+            D_day += m_list[m]
+        D_day += (d2 - d1 + 1)
+    elif d1 > d2:
+        for m in range(m1, m2):
+            D_day += m_list[m]
+        D_day += (d2 - d1 + 1)
     if d1 < d2:
         D_day += (d2 - d1)
 elif m1 > m2:
-    D_day += (y2 - y1) * 365
-    for m in range(m1, 13):
-        D_day += m_list[m]
-    for m in range(1, m2):
-        D_day += m_list[m]
+    D_day += (y2 - y1 - 1) * 365
     if d1 < d2:
-        D_day += (d2 - d1)
+        for m in range(m1, 13):
+            D_day += m_list[m]
+        for m in range(1, m2):
+            D_day += m_list[m]
+        D_day += (d2 - d1 + 1)
     elif d1 > d2:
-        D_day += (m_list[m1]+1-d2)+d2
+        for m in range(m1, 13):
+            D_day += m_list[m]
+        for m in range(1, m2):
+            D_day += m_list[m]
+        D_day += 1-d1+d2
 
 print(D_day)
