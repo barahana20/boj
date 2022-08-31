eba9d69611cffb8aeace8df69d393fff3a493b68
bara <barahana13@naver.com>
2022-08-31 15:30:00 +0900

    auto push

--- BOJ/1308.py
+++ BOJ/1308.py
@@ -1,3 +1,24 @@
 y1,m1,d1 = map(int, input().split())
 y2,m2,d2 = map(int, input().split())
+D_day = 0
+m_list = {1:31, 2:28, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:31, 12:30}
+m_list1 = {1:31, 2:29, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:31, 12:30} # 윤년
 
+if m1 < m2:
+    D_day += (y2 - y1) * 365
+    for m in range(m1, m2):
+        D_day += m_list[m]
+    if d1 < d2:
+        D_day += (d2 - d1)
+elif m1 > m2:
+    D_day += (y2 - y1) * 365
+    for m in range(m1, 13):
+        D_day += m_list[m]
+    for m in range(1, m2):
+        D_day += m_list[m]
+    if d1 < d2:
+        D_day += (d2 - d1)
+    elif d1 > d2:
+        D_day += (m_list[m1]+1-d2)+d2
+
+print(D_day)
